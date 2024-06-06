a88b0ff42bdec085ced035a92304970f36b524e2
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-06 17:57:24 +0200

    did more work on stuff

--- src/page-facts.php
+++ src/page-facts.php
@@ -36,9 +36,9 @@
 <section class="hard-facts-first">
 <div class="hard-facts-first-container">
     <div class="hard-facts-first-text">
-        <h2>Freier Eintritt</h2>
+        <h2>FREIER EINTRITT</h2>
         <p>Das Kunstmue Festival bleibt weiterhin kostenfrei und wird vollständig von freiwilligen Helfern organisiert. Alle Einnahmen aus dem Verkauf von Getränken und Merchandise fließen in die Finanzierung des nächsten Festivals. Wir begrüßen jedoch gerne freiwillige Spenden am Eingang.</p>
-        <p>Wichtiger Hinweis: Aus Gründen des Jugendschutzes werden am Eingang für die Besucher je nach Alter verschiedenfärbige Armbänder ausgegeben. Das Mitbringen von Getränken auf das Festivalgelände ist nicht gestattet, ausgenommen ist der Campingbereich.</p>    
+        <p><span class="bold">Wichtiger Hinweis:</span> Aus Gründen des Jugendschutzes werden am Eingang für die Besucher je nach Alter verschiedenfärbige Armbänder ausgegeben. Das Mitbringen von Getränken auf das Festivalgelände ist nicht gestattet, ausgenommen ist der Campingbereich.</p>    
     </div>
     <img src="<?php echo get_template_directory_uri(); ?>/images/eingang.jpg"" alt="Freier Eintritt">
 </div>
@@ -78,6 +78,40 @@
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6394.427288725513!2d13.620197179469779!3d47.63535434091935!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47714ba8950ae2df%3A0x3615cd61b060e599!2sKunstmue%20Festival!5e0!3m2!1sde!2sat!4v1716211962402!5m2!1sde!2sat" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
+
+<section class="hard-facts-fifth-container">
+    <div class="hard-facts-fifth">
+    <h2>ANREISE</h2>
+    <p>Das Kunstmue Festival Gelände kann gut mit den öffentlichen Verkehrsmitteln erreicht werden. Wir bitten um eine umweltschonende Anreise:</p>
+<div class="timeline-container">
+        <div class="anreiseart">
+            <h3>
+                ZUG
+            </h3>
+            <h3>
+                BUS
+            </h3>
+            <h3>
+                AUTO
+            </h3>
+        </div>
+        <div class="anreiseding">
+        <img class="anreisegrafik" src="<?php echo get_template_directory_uri(); ?>/images/anreise-ding.svg" alt="Anreise">
+        </div>
+        <div class="anreisebeschreibung">
+            <p>
+            Aussteigen in Bhf. Bad Goisern. Vom Bahnhofsplatz aus bei der Straßengabelung die rechte Straße entlang bis zur Brücke über den Mühlbach, unmittelbar danach rechts. Etwa 250 bis 300 Meter geradeaus, dann rechts, über die Goiserer Brücke, nach der Brücke gleich links. Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht.
+            </p>
+            <p>
+            Haltestelle: Bad Goisern Mitte / B145. Von der Bushaltestelle Richtung Bad Goisern Zentrum und von dort links halten. Weiter bis zur evangelischen Kirche, vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert.
+            </p>
+            <p>
+            Anfahrt über B 145, Ortseinfahrt Bad Goisern Mitte (Kreisverkehr beim Hofer Supermarkt), ins Zentrum bis zur großen Kreuzung. Dort geradeaus, an der evangelischen Kirche vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert. Begrenzte Parkmöglichkeiten gibt es am Straßenrand der Hinlaufstraße.
+            </p>
+        </div>
+</div>
+</div>
+</section>
   </main>
  
 
