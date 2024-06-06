20ef4e9aa2d57698f34aee3f3f07432a42e17778
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-06 14:04:36 +0200

    added responsiveness to front page and header - burger menu

--- src/php/header.php
+++ src/php/header.php
@@ -20,7 +20,31 @@
 <?php wp_body_open(); ?>
   <header>
     <div class="nav">
-      <div class="logo"><a href="/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Logo"></a></div></li>
+      <div class="logo"><a href="/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Logo"></a></div>
+      <div class="burger" onclick="openMenu()"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/Burger_menue.svg" alt="Burger"></div>
+      <div class="close hidden" onclick="closeMenu()"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/Close.svg" alt="Close"></div>
+     <div id="menu" class="hidden">
+      <div class="menu-container">
       <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+      </div>
+      </div>
     </div>
 </header>
+
+<script>
+  const burger = document.querySelector('.burger');
+  const close = document.querySelector('.close');
+  const hidden = document.querySelector('.hidden');
+  const menu = document.querySelector('#menu');
+  const openMenu = () => {
+        menu.classList.remove('hidden');
+        close.classList.remove('hidden');
+        burger.classList.add('hidden');
+    }
+
+    const closeMenu = () => {
+        menu.classList.add('hidden');
+        close.classList.add('hidden');
+        burger.classList.remove('hidden');
+    }
+</script>
