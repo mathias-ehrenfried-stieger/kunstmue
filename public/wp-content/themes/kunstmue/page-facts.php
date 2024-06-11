22e6bd83fcccb44a04882eacd455756e8bf731bb
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-11 17:52:03 +0200

    finished facts on mobile for now and ironed out some things on desktop

--- public/wp-content/themes/kunstmue/page-facts.php
+++ public/wp-content/themes/kunstmue/page-facts.php
@@ -1,162 +1,163 @@
-<?php 
+<?php
 /* Template Name: facts */
 ?>
 
-  <?php get_header(); ?>
-  <main>
+<?php get_header(); ?>
+<main>
     <section class="fact-hero">
-    <h1>HARD FACTS</h1>
-    <div class="hero-fact-text">
-        <div>
-            <p>
-                2 TAGE
-            </p>
-            <p>
-                12 BANDS
-            </p>
+        <h1>HARD FACTS</h1>
+        <div class="hero-fact-text">
+            <div>
+                <p>
+                    2 TAGE
+                </p>
+                <p>
+                    12 BANDS
+                </p>
+            </div>
+            <div>
+                <p>
+                    FREIER
+                </p>
+                <p>
+                    EINTRITT
+                </p>
+            </div>
+            <div>
+                <p>
+                    GRATIS
+                </p>
+                <p>
+                    CAMPING
+                </p>
+            </div>
         </div>
-        <div>
-            <p>
-                FREIER
-            </p>
-            <p>
-                EINTRITT
-            </p>
+    </section>
+    <section class="hard-facts-first">
+        <div class="hard-facts-first-container">
+            <div class="hard-facts-first-text">
+                <h2>FREIER EINTRITT</h2>
+                <p>Das Kunstmue Festival bleibt weiterhin kostenfrei und wird vollständig von freiwilligen Helfern organisiert. Alle Einnahmen aus dem Verkauf von Getränken und Merchandise fließen in die Finanzierung des nächsten Festivals. Wir begrüßen jedoch gerne freiwillige Spenden am Eingang.</p>
+                <p><span class="bold">Wichtiger Hinweis:</span> Aus Gründen des Jugendschutzes werden am Eingang für die Besucher je nach Alter verschiedenfärbige Armbänder ausgegeben. Das Mitbringen von Getränken auf das Festivalgelände ist nicht gestattet, ausgenommen ist der Campingbereich.</p>
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/eingang.jpg"" alt=" Freier Eintritt">
         </div>
-        <div>
-            <p>
-                GRATIS
-            </p>
-            <p>
-                CAMPING
-            </p>
+    </section>
+    <section class="hard-facts-second">
+        <div class="hard-facts-second-teaser">
+            <h2>GRATIS CAMPING</h2>
+            <p><span class="bold">Camping am Kunstmue Festival ist gratis.</span> Die Campingmöglichkeiten sind nicht weit vom Eingangsbereich. Die schattigen Zeltplätze findet ihr in einem kleinen Waldstück neben der erfrischenden Traun (Bademöglichkeit!). Außerdem stehen kostenlose Caravanplätze zur Verfügung.</p>
+            <p><span class="bold">Anmeldung vor Ort:</span> Anreise ist ab 14:00 Uhr möglich. Bitte kommt bei Ankunft zum Eingang, hier könnt ihr den Meldezettel ausfüllen und ihr bekommt kostenfreie Müllsäcke, welche ihr nach dem Festival wieder abgeben könnt. Camping beim Kunstmue Festival ist Ortstaxen befreit.</p>
+            <p><span class="bold">Wichtiger Hinweis:</span> Bitte beachtet, dass absolutes Feuerverbot herrscht!</p>
         </div>
-    </div>
-</section>
-<section class="hard-facts-first">
-<div class="hard-facts-first-container">
-    <div class="hard-facts-first-text">
-        <h2>FREIER EINTRITT</h2>
-        <p>Das Kunstmue Festival bleibt weiterhin kostenfrei und wird vollständig von freiwilligen Helfern organisiert. Alle Einnahmen aus dem Verkauf von Getränken und Merchandise fließen in die Finanzierung des nächsten Festivals. Wir begrüßen jedoch gerne freiwillige Spenden am Eingang.</p>
-        <p><span class="bold">Wichtiger Hinweis:</span> Aus Gründen des Jugendschutzes werden am Eingang für die Besucher je nach Alter verschiedenfärbige Armbänder ausgegeben. Das Mitbringen von Getränken auf das Festivalgelände ist nicht gestattet, ausgenommen ist der Campingbereich.</p>    
-    </div>
-    <img src="<?php echo get_template_directory_uri(); ?>/images/eingang.jpg"" alt="Freier Eintritt">
-</div>
-</section>
-<section class="hard-facts-second">
-    <div class="hard-facts-second-teaser">
-    <h2>GRATIS CAMPING</h2>
-    <p>Camping am Kunstmue Festival ist gratis.Die Campingmöglichkeiten sind nicht weit vom Eingangsbereich. Die schattigen Zeltplätze findet ihr in einem kleinen Waldstück neben der erfrischenden Traun (Bademöglichkeit!). Außerdem stehen kostenlose Caravanplätze zur Verfügung.</p>
-    <p>Anmeldung vor Ort:Anreise ist ab 14:00 Uhr möglich. Bitte kommt bei Ankunft zum Eingang, hier könnt ihr den Meldezettel ausfüllen und ihr bekommt kostenfreie Müllsäcke, welche ihr nach dem Festival wieder abgeben könnt. Camping beim Kunstmue Festival ist Ortstaxen befreit.</p>
-    <p>Wichtiger Hinweis: Bitte beachtet, dass absolutes Feuerverbot herrscht!</p>
-    </div>
-    <div class="hard-facts-second-container">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/camping.jpg" alt="Camping">
-        <div class="hard-facts-first-text">
-            <h2>GRATIS CARAVAN</h2>
-            <p class="fake-button">Jetzt Caravanplatz sichern!</p>
-            
+        <div class="hard-facts-second-container">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/camping.jpg" alt="Camping">
+            <div class="hard-facts-first-text">
+                <h2>GRATIS CARAVAN</h2>
+                <p class="fake-button">Jetzt Caravanplatz sichern!</p>
+
                 <p>Sende eine Email mit folgenden Infos an: kunstmueverein@gmail.com</p>
                 <ul>
                     <li>Name</li>
                     <li>Anzahl der Personen</li>
-                    <li>Anzahl der Caravans</li>    
+                    <li>Anzahl der Caravans</li>
                 </ul>
                 <p>Ansprechperson: Julia Pomberger Bei Fragen vor Ort, gerne einfach anrufen! +43 676 4355456</p>
                 <p>Sollte ein angemeldeter Platz doch nicht in Anspruch genommen werden, bitten wir um eine kurze Abmeldung.</p>
             </div>
-    </div>
-</section>
-<section class="hard-facts-third">
-<img class="plan" src="<?php echo get_template_directory_uri(); ?>/images/gelaendeplan.png" alt="Geländeplan">
-<a class="btn" href="<?php echo get_template_directory_uri(); ?>/images/gelaendeplan.png" download>Download Geländeplan</a> 
+        </div>
+    </section>
+    <section class="hard-facts-third">
+        <img class="plan" src="<?php echo get_template_directory_uri(); ?>/images/gelaendeplan.png" alt="Geländeplan">
+        <a class="btn" href="<?php echo get_template_directory_uri(); ?>/images/gelaendeplan.png" download>Download Geländeplan</a>
 
-</section>
-<section class="hard-facts-fourth">
-    <div class="hard-facts-fourth-content">
-    <h2>ANFAHRT</h2>
-    <p>Das Kunstmue Festival findet im malerischen Herzen des Salzkammerguts statt. Eingebettet in eine atemberaubende Bergkulisse und entlang des Traunflusses, der an warmen Tagen für erfrischende Abkühlung sorgt, bietet dieser Veranstaltungsort ein einzigartiges Festivalerlebnis.</p>
-    <p>Adresse: Gschwandt 115, 4822 Bad Goisern am Hallstättersee</p>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6394.427288725513!2d13.620197179469779!3d47.63535434091935!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47714ba8950ae2df%3A0x3615cd61b060e599!2sKunstmue%20Festival!5e0!3m2!1sde!2sat!4v1716211962402!5m2!1sde!2sat" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+    <section class="hard-facts-fourth">
+        <div class="hard-facts-fourth-content">
+            <h2>ANFAHRT</h2>
+            <p>Das Kunstmue Festival findet im malerischen Herzen des Salzkammerguts statt. Eingebettet in eine atemberaubende Bergkulisse und entlang des Traunflusses, der an warmen Tagen für erfrischende Abkühlung sorgt, bietet dieser Veranstaltungsort ein einzigartiges Festivalerlebnis.</p>
+            <p>Adresse: Gschwandt 115, 4822 Bad Goisern am Hallstättersee</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6394.427288725513!2d13.620197179469779!3d47.63535434091935!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47714ba8950ae2df%3A0x3615cd61b060e599!2sKunstmue%20Festival!5e0!3m2!1sde!2sat!4v1716211962402!5m2!1sde!2sat" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 
-<section class="hard-facts-fifth-container">
-    <div class="hard-facts-fifth">
-    <h2>ANREISE</h2>
-    <p>Das Kunstmue Festival Gelände kann gut mit den öffentlichen Verkehrsmitteln erreicht werden. Wir bitten um eine umweltschonende Anreise:</p>
-<div class="timeline-container">
-        <div class="anreiseart">
-            <h3>
-                ZUG
-            </h3>
-            <h3>
-                BUS
-            </h3>
-            <h3>
-                AUTO
-            </h3>
+    <section class="hard-facts-fifth-container">
+        <div class="hard-facts-fifth">
+            <h2>ANREISE</h2>
+            <p>Das Kunstmue Festival Gelände kann gut mit den öffentlichen Verkehrsmitteln erreicht werden. Wir bitten um eine umweltschonende Anreise:</p>
+            <div class="timeline-container">
+                <div class="anreiseart">
+                    <h3>
+                        ZUG
+                    </h3>
+                    <h3>
+                        BUS
+                    </h3>
+                    <h3>
+                        AUTO
+                    </h3>
+                </div>
+                <div class="anreiseding">
+                    <img class="anreisegrafik" src="<?php echo get_template_directory_uri(); ?>/images/anreise-ding.svg" alt="Anreise">
+                </div>
+                <div class="anreisebeschreibung">
+                    <p>
+                        Aussteigen in Bhf. Bad Goisern. Vom Bahnhofsplatz aus bei der Straßengabelung die rechte Straße entlang bis zur Brücke über den Mühlbach, unmittelbar danach rechts. Etwa 250 bis 300 Meter geradeaus, dann rechts, über die Goiserer Brücke, nach der Brücke gleich links. Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht.
+                    </p>
+                    <p>
+                        Haltestelle: Bad Goisern Mitte / B145. Von der Bushaltestelle Richtung Bad Goisern Zentrum und von dort links halten. Weiter bis zur evangelischen Kirche, vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert.
+                    </p>
+                    <p>
+                        Anfahrt über B 145, Ortseinfahrt Bad Goisern Mitte (Kreisverkehr beim Hofer Supermarkt), ins Zentrum bis zur großen Kreuzung. Dort geradeaus, an der evangelischen Kirche vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert. Begrenzte Parkmöglichkeiten gibt es am Straßenrand der Hinlaufstraße.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="anreiseding">
-        <img class="anreisegrafik" src="<?php echo get_template_directory_uri(); ?>/images/anreise-ding.svg" alt="Anreise">
+    </section>
+    <section class="facts-parking">
+        <div class="facts-parking-container">
+            <h2>PARKMÖGLICHKEITEN</h2>
+            <img class="park-plan" src="<?php echo get_template_directory_uri(); ?>/images/parking.png" alt="Parkinglot">
         </div>
-        <div class="anreisebeschreibung">
-            <p>
-            Aussteigen in Bhf. Bad Goisern. Vom Bahnhofsplatz aus bei der Straßengabelung die rechte Straße entlang bis zur Brücke über den Mühlbach, unmittelbar danach rechts. Etwa 250 bis 300 Meter geradeaus, dann rechts, über die Goiserer Brücke, nach der Brücke gleich links. Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht.
-            </p>
-            <p>
-            Haltestelle: Bad Goisern Mitte / B145. Von der Bushaltestelle Richtung Bad Goisern Zentrum und von dort links halten. Weiter bis zur evangelischen Kirche, vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert.
-            </p>
-            <p>
-            Anfahrt über B 145, Ortseinfahrt Bad Goisern Mitte (Kreisverkehr beim Hofer Supermarkt), ins Zentrum bis zur großen Kreuzung. Dort geradeaus, an der evangelischen Kirche vorbei bis zur Goiserer Brücke (über die Traun), nach der Brücke gleich links, Ca. 300 bis 400 Meter geradeaus, bis auf der linken Seite die Kunstmühle auftaucht. Der Weg zum Festival ist ab der Ortseinfahrt beschildert. Begrenzte Parkmöglichkeiten gibt es am Straßenrand der Hinlaufstraße.
-            </p>
-        </div>
-</div>
-</div>
-</section>
-<section class="facts-parking">
-    <div class="facts-parking-container">
-        <h2>PARKMÖGLICHKEITEN</h2>
-        <img class="plan" src="<?php echo get_template_directory_uri(); ?>/images/parking.png" alt="Parkinglot">
-    </div>
 
-</section>
-<section class="facts-foodcorner">
-    <div class="facts-foodcorner-container">
-        <h2>FOODCORNER DER D´SPEIS</h2>
-        <img class="plan" src="<?php echo get_template_directory_uri(); ?>/images/parking.png" alt="Parkinglot">
-        <p>In unserem erweitertem Foodcorner findet ihr heuer Foodtrucks von d’Speis aus St. Wolfgang. Sandro, Eigentümer und Koch mit Herz und Seele verwöhnt euch mit frischen, regionalen und ausgewählten Bio Produkten. Qualitätsfleisch aus der Region und vegetarischen sowie vegane Gerichte stehen auf der Speisekarte.
-        Lasst euch kulinarisch verwöhnen!</p>
-    
-    <div class="facts-foodcorner-below">
-        <div class="facts-foodcorner-below-text">
-        <h2>Frühstücken & Frühshoppen</h2>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.</p>
+    </section>
+    <section class="facts-foodcorner">
+        <div class="facts-foodcorner-container">
+            <h2>FOODCORNER DER D´SPEIS</h2>
+            <img class="park-plan" src="<?php echo get_template_directory_uri(); ?>/images/parking.png" alt="Parkinglot">
+            <p>In unserem erweitertem Foodcorner findet ihr heuer Foodtrucks von d’Speis aus St. Wolfgang. Sandro, Eigentümer und Koch mit Herz und Seele verwöhnt euch mit frischen, regionalen und ausgewählten Bio Produkten. Qualitätsfleisch aus der Region und vegetarischen sowie vegane Gerichte stehen auf der Speisekarte.
+                Lasst euch kulinarisch verwöhnen!</p>
+
+            <div class="facts-foodcorner-below">
+                <div class="facts-foodcorner-below-text">
+                    <h2>Frühstücken & Frühshoppen</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.</p>
+                </div>
+                <img class="img-foodcorner" src="<?php echo get_template_directory_uri(); ?>/images/eingang.jpg"" alt=" Freier Eintritt">
+            </div>
         </div>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/eingang.jpg"" alt="Freier Eintritt">
-    </div>
-    </div>
-</section>
+    </section>
 
-<section class="facts-kinderprogramm">
-    <div class="facts-kinderprogramm-container">
-<img class="img-kinderprogramm" src="<?php echo get_template_directory_uri(); ?>/images/Kinderprogram.jpg"" alt="Freier Eintritt">
-<div class="facts-kinderprogramm-text">
-<h2>KINDERPROGRAMM</h2>
-<p>Das Kunstmue ist ein Festival für die ganze Familie. Für die Kleinen bieten wir  ein umfangreiches Kinderprogramm ab 14:00 Uhr in Kooperation mit der Freiwilligen Feuerwehr Bad Goisern:</p>
-<ul>
-    <li>Nostalgiespritze</li>
-    <li>Kinderschminken</li>
-    <li>Farbschleuder- T-Shirts bedrucken</li>
-    <li>Verschiedene Spiele: Malbilder, Luftballons uvm.</li>
-</ul>
-</div>
-</div>
-</section>
-  </main>
- 
+    <section class="facts-kinderprogramm">
+        <div class="facts-kinderprogramm-container">
+            <img class="img-kinderprogramm" src="<?php echo get_template_directory_uri(); ?>/images/Kinderprogram.jpg"" alt=" Freier Eintritt">
+            <div class="facts-kinderprogramm-text">
+                <h2>KINDERPROGRAMM</h2>
+                <p>Das Kunstmue ist ein Festival für die ganze Familie. Für die Kleinen bieten wir ein umfangreiches Kinderprogramm ab 14:00 Uhr in Kooperation mit der Freiwilligen Feuerwehr Bad Goisern:</p>
+                <ul>
+                    <li>Nostalgiespritze</li>
+                    <li>Kinderschminken</li>
+                    <li>Farbschleuder- T-Shirts bedrucken</li>
+                    <li>Verschiedene Spiele: Malbilder, Luftballons uvm.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</main>
 
-  <?php get_footer(); ?>
+
+<?php get_footer(); ?>
 
 </body>
+
 </html>
--- src/page-agb.php
+++ src/page-agb.php
@@ -1,30 +1,31 @@
-<?php 
+<?php
 /* Template Name: agb */
 ?>
 
 <?php get_header(); ?>
-  <main>
-    <section class="imprint-hero-container">
-        <div class="imprint-hero">
-    <h1>AGBs</h1>
+<main>
+  <section class="imprint-hero-container">
+    <div class="imprint-hero">
+      <h1>AGBs</h1>
 
-    <div>
-    <p class="bold">Kunstmue Veranstaltungsverein</p>
-    <p>Lasern 106</p>
-    <p>4822 Bad Goisern</p>
-    <p>Österreich</p>
-    </div>
+      <div>
+        <p class="bold">Kunstmue Veranstaltungsverein</p>
+        <p>Lasern 106</p>
+        <p>4822 Bad Goisern</p>
+        <p>Österreich</p>
+      </div>
 
-    <div>
-    <p class="bold">Kontakt</p>
-    <p>kunstmueverein@gmail.com</p>
+      <div>
+        <p class="bold">Kontakt</p>
+        <p>kunstmueverein@gmail.com</p>
+      </div>
     </div>
-    </div>
-    </section>
-  </main>
- 
+  </section>
+</main>
 
-  <?php get_footer(); ?>
+
+<?php get_footer(); ?>
 
 </body>
+
 </html>
