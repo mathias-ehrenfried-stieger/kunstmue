3794ccfd57a6ba62d0d9f43c3fd65d7283974c39
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-26 15:08:10 +0200

    partner-shennanigans

--- src/page-partner.php
+++ src/page-partner.php
@@ -4,11 +4,40 @@
 
 <?php get_header(); ?>
   <main>
-  <section class="about-hero">
-    <h1>Work in Progress</h1>
-    <p>Wir arbeiten gerade an dieser Seite. Bald findest du hier mehr Informationen über uns.</p>
-    </section>
+  <section class="partner-hero">
+    <div class="partner-hero-text">
+    <h1>PARTNER*INNEN</h1>
+    <h2>WIR SAGEN DANKE</h2>
+    <p>Wir möchten uns herzlich bei  unseren zahlreichen Sponsor*innen und Partner*innen bedanken, die das Kunstmue Festival unterstützen.</p>
+    </div>
+  </section>
+  <section class="partner-logo">
+    <div class="partner-logo-container">
+      
+      <img class="umwelt-img" src="<?php echo get_template_directory_uri(); ?>/images/logos/umwelt.png" alt="logos">
+      
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/Feuerwehr.JPG" alt="logos">
+      
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/goisern.jpg" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/green.jpg" alt="logos">
+  
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/dachstein.png" alt="logos">
+  
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/frskgt.jpg" alt="logos">
+  
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/Raiffeisen.JPG" alt="logos">
+  
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/workwear.png" alt="logos">
 
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/stiegl.gif" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/pomberger.jpg" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/zebau.jpg" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/eddi.png" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/CG.jpg" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/wärme.jpg" alt="logos">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/ArtwLogo.png" alt="logos">
+  </div>
+  </section>
   </main>
  
 
