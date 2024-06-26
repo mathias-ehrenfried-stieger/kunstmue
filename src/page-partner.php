12663eae0892f45fccc3468f8decd812bc86cccf
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-26 15:58:19 +0200

    changed logos

--- src/page-partner.php
+++ src/page-partner.php
@@ -14,28 +14,39 @@
   <section class="partner-logo">
     <div class="partner-logo-container">
       
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/green.jpg" alt="logos">
       <img class="umwelt-img" src="<?php echo get_template_directory_uri(); ?>/images/logos/umwelt.png" alt="logos">
       
       <img src="<?php echo get_template_directory_uri(); ?>/images/logos/Feuerwehr.JPG" alt="logos">
-      
       <img src="<?php echo get_template_directory_uri(); ?>/images/logos/goisern.jpg" alt="logos">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/green.jpg" alt="logos">
   
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/dachstein.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/dachstein.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/frskgt.jpg" alt="logos">
   
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/frskgt.jpg" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/stiegl.gif" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/pomberger.jpg" alt="logos">
+      
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/Raiffeisen.JPG" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/workwear.png" alt="logos">
   
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/Raiffeisen.JPG" alt="logos">
-  
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/workwear.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/ArtwLogo.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/zebau.jpg" alt="logos">
 
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/stiegl.gif" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/pomberger.jpg" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/zebau.jpg" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/eddi.png" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/CG.jpg" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/wärme.jpg" alt="logos">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/logos/ArtwLogo.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/wärme.jpg" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/greenroof.png" alt="logos">
+
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/rebel.jpg" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/ronacher.jpg" alt="logos">
+
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/dischgu.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/romana.jpg" alt="logos">
+
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/kurtis.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/maislinger_logo.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/traudi.png" alt="logos">
+
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/eddi.png" alt="logos">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logos/CG.jpg" alt="logos">
   </div>
   </section>
   </main>
