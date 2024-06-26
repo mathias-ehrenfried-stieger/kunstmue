d8d1ef3bce27f1e041b3d8ff65bcf51459b2c464
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-06-26 10:15:47 +0200

    added wip pages

--- public/wp-content/themes/kunstmue/page-partner.php
+++ public/wp-content/themes/kunstmue/page-partner.php
@@ -4,7 +4,10 @@
 
 <?php get_header(); ?>
   <main>
+  <section class="about-hero">
     <h1>Work in Progress</h1>
+    <p>Wir arbeiten gerade an dieser Seite. Bald findest du hier mehr Informationen über uns.</p>
+    </section>
 
   </main>
  
