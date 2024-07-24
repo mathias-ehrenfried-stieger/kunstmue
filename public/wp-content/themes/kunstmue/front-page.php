b8adb2f1fd8554782ba51fd9f53994c418fce6f5
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2024-07-24 12:54:33 +0200

    added programm

--- public/wp-content/themes/kunstmue/front-page.php
+++ public/wp-content/themes/kunstmue/front-page.php
@@ -3,11 +3,71 @@
   <?php get_header(); ?>
   <main>
     <section class="hero"></section>
+
+    <section class="program">
+    <div class="front-page-container">
+      <h2>Programm</h2>
+      <p class="underlined">Auch heuer haben wir wieder neben musikalischer Unterhaltung ein umfangfreiches Rahmenprogramm für euch:</p>
+      <h3>Frühschoppen mit Live Volksmusik 
+      & Kinderprogramm </h3>
+      <p>Samstag, 26. Juli 2024</p>
+      <p>ab 10:00 Uhr: Frühschoppen</p>
+      <p>ab 14:00 Uhr: Kinderprogramm</p>
+      <h4>Frühshoppen </h4>
+      <p class="underlined">Live Volksmusik & freies Musizieren</p>
+      <p>Volksmusikanten sind gerne eingeladen mitzumachen</p>
+      <p class="underlined">Verpflegung </p>
+      <div>
+      <p>Kaffee & Kuchenbuffet</p>
+      <p>Kesselheisse mit diversen Würstel und Gebäck</p>
+      <p>Eierspeis mit regionalen Eiern</p>
+      <p>Frisches Gebäck von Kurtis Cafe & Bäckerei</p>
+      <p>diverse vegane und vegetarische Aufstriche</p>
+      </div>
+      <h4>Kinderprogramm</h4>
+      <p>In Kooperation mit der Freiwilligen Feuerwehr Bad Goisern: </p>
+      <ul>
+        <li>Nostalgiespritze </li>
+        <li>Kinderschminken</li>
+        <li>Farbschleuder – T-Shirts bedrucken </li>
+        <li>Verschiedene Spiele: Malbilder, Luftballons uvm.</li>
+      </ul>
+      
+     <a class="btn" href="<?php echo get_permalink( get_page_by_title( 'LINE-UP' ) ); ?>">Hier gehts zum Line-Up</a>
+     </div>
+    </section>
+    <section class="green-event">
+      <div class="front-page-container">
+      <h2>Das Kunstmue Festival ist ein offizielles Green Event:</h2>
+      <p>Bereits in den letzten Jahren haben wir auf den ressourcenschonenden Einsatz von Material, Wasser und Strom sowie auf die Vermeidung von Müll geachtet. Nun möchten wir unsere soziale und ökologische Verantwortung noch ernster nehmen und haben uns zum Green Event zertifizieren lassen. </p>
+      <h3>Was wir machen:</h3>
+      <p class="underlined">Klimaschonende Anreise:</p>
+      <p>Wir informieren euch über klimaschonende Anreise auf unserer Website und Social Media. Ihr könnt uns zu Fuß, mit dem Rad oder mit öffentlichen Verkehrsmittel gut erreichen. </p>
+      <p class="underlined">Ressourcenschonender Verbrauch:</p>
+      <p>Strom, Wasser, Energie und Material wird sorgsam verwendet. Alle Druckwerke wurden in geringer Auflage und teils auf umweltfreundlichem Papier gedruckt. </p>
+      <p class="underlined">Abfallvermeidung und -trennung:</p>
+      <p>Wir verwenden Mehrweggeschirr, Mehrwegbecher & -verpackungen. Es gibt ein Müllkonzept, welches dafür sorgt, dass jeglicher Abfall getrennt gesammelt wird. Camping Gäste bekommen kostenlose Müllsäcke, welche nach dem Festival wieder abgegeben werden können.</p>
+      <p class="underlined">Essen & Getränke:</p>
+      <p>Bio- und Fairtrade sowie regionale und saisonale Produkte, vegetarische & vegane Speisen werden geboten.</p>
+      <p class="underlined">Soziale Barrieren: </p>
+      <p>Wie immer gibt es freien Eintritt & gratis Camping. Gerne ist die Crew Menschen mit körperlichen Einschränkungen behilflich! Zudem haben wir heuer ein barrierefreies WC am Gelände. Wenn ihr euch unwohl oder belästigt fühlt, wendet euch an unsere Awareness Person Chrissi am Merchandise Stand.</p>
+      <h3>Was ihr machen könnt:</h3>
+      <p class="underlined">Klimaschonende Anreise:</p>
+      <p>Kommt zu Fuß, mit dem Rad, mit öffentlichen Verkehrsmitteln oder bildet Fahrgemeinschaften. </p>
+      <p class="underlined">Abfallvermeidung und -trennung:</p>
+      <p>Wir bitten um Abfallvermeidung und richtige Mülltrennung. Verwendet die ausgegebenen Müllsäcke am Campingplatz. Am Campingplatz herrscht absolutes Feuerverbot.</p>
+      <p class="underlined">Essen & Getränke:</p>
+      <p>Gönnt euch doch mal was Vegetarisches oder Veganes. Versucht unsere Bio Getränke und Speisen.</p> 
+      <p class="underlined">Soziale Barrieren:  </p>
+      <p>Wir bitten um respektvolles Miteinander! Wenn jemand Hilfe benötigt, gebt uns gerne Bescheid. Wenn ihr euch unwohl oder belästigt fühlt, endet euch an Chrissi, unsere Awareness Person am Merch Stand. </p>  
+      </div>
+    </section>
     <section class="below">
       <h1>DAS WAR DAS KUNSTMUE FESTIVAL 2023</h1>
       <div class="video-container">
         <iframe src="https://www.youtube.com/embed/omfoFYQHndI?si=c2Wr2rtZMKmyW_rg" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
       </div>
+
     </section>
 
   </main>
