1acf9123739151fae794fa9bfa3193258f2b4292
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2025-01-17 16:43:06 +0100

    fix some shite

--- public/wp-content/themes/kunstmue/front-page.php
+++ public/wp-content/themes/kunstmue/front-page.php
@@ -16,44 +16,14 @@
         <p>Auch heuer rocken wieder zahlreiche Bands verschiedenster Genres die Festival Bühne, zudem dürft ihr
 euch wieder auf das Frühschoppen mit unseren örtlichen Volksmusikanten, 
 ein buntes Kinderprogramm, köstliche regionale Speisen 
-– inklusive vegetarischer und veganer Optionen – und eine einzigartige Festivalvibes freuen!
+– inklusive vegetarischer und veganer Optionen – und einzigartige Festivalvibes freuen!
 Bleibt dran für weitere Updates und seid gespannt auf das, was euch 2025 erwartet!</p>
-        <a class="btn" href="<?php echo get_permalink( get_page_by_title( 'FACTS' ) ); ?>">Mehr über uns</a>
+<?php
+  $query = new WP_Query( array( 'pagename' => 'FACTS' ) );
+?>
+        <a class="btn" href="<?php echo get_permalink( $query->post ); ?>">Mehr über uns</a>
       </div>
     </section>
-    <!-- <section class="program">
-    <div class="front-page-container">
-      <h2>Programm</h2>
-      <p class="underlined">Auch heuer haben wir wieder neben musikalischer Unterhaltung ein umfangfreiches Rahmenprogramm für euch:</p>
-      <h3>Frühschoppen mit Live Volksmusik 
-      & Kinderprogramm </h3>
-      <p>Samstag, 26. Juli 2024</p>
-      <p>ab 10:00 Uhr: Frühschoppen</p>
-      <p>ab 14:00 Uhr: Kinderprogramm</p>
-      <h4 class="underlined bigAndBold">Frühshoppen </h4>
-      <p>Live Volksmusik & freies Musizieren</p>
-      <p>Volksmusikanten sind gerne eingeladen mitzumachen</p>
-      <p >Verpflegung </p>
-      <div>
-      <p>Kaffee & Kuchenbuffet</p>
-      <p>Kesselheisse mit diversen Würstel und Gebäck</p>
-      <p>Eierspeis mit regionalen Eiern</p>
-      <p>Frisches Gebäck von Kurtis Cafe & Bäckerei</p>
-      <p>diverse vegane und vegetarische Aufstriche</p>
-      </div>
-      <h4 class="underlined bigAndBold">Kinderprogramm</h4>
-      <p>In Kooperation mit der Freiwilligen Feuerwehr Bad Goisern: </p>
-      <ul>
-        <li>Nostalgiespritze </li>
-        <li>Kinderschminken</li>
-        <li>Farbschleuder – T-Shirts bedrucken </li>
-        <li>Verschiedene Spiele: Malbilder, Luftballons uvm.</li>
-      </ul>
-      
-     <a class="btn" href="<?php echo get_permalink( get_page_by_title( 'LINE-UP' ) ); ?>">Hier gehts zum Line-Up</a>
-     </div>
-    </section> -->
-   
     <section class="below">
       <h1>DAS WAR DAS KUNSTMUE FESTIVAL 2024</h1>
       <div class="video-container">
