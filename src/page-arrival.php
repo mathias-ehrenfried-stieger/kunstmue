e5c58e44c2ef5cfa81a57c39d1413ba66ec5f4fe
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2025-04-17 23:11:58 +0200

    new wp-version

---
+++ src/page-arrival.php
@@ -0,0 +1,20 @@
+<?php
+/* Template Name: anreise */
+?>
+
+<?php get_header(); ?>
+<main>
+    <section class="about-hero">
+        <h1>Kunstmue Festival Bad Goisern<br>
+            Free Alternative Music Festival</h1>
+    </section>
+    <h2>Hello WOrld</h2>
+
+</main>
+
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
