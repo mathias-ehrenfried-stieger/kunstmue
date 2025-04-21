1b6754b1e638ffe85851d75664503213d096678c
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2025-04-21 10:35:38 +0200

    add contact form tests

---
+++ src/page-contact-form.php
@@ -0,0 +1,11 @@
+<?php 
+/* Template Name: contact form */
+get_header(); ?>
+
+    <main id="form">
+        <?php
+
+    echo do_shortcode('[contact-form-7 id="dd8503d" title="Contact form 1"]');
+   ?>
+    </main>
+    <?php get_footer(); ?>
