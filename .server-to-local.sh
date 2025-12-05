0653aa9a6c043623c05d8a3a710d97849289c70d
Mathias Ehrenfried Stieger <mstieger.mmt-b2022@fh-salzburg.ac.at>
2025-12-05 13:53:46 +0100

    db-sync

---
+++ .server-to-local.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#!/usr/bin/env bash
+
+
+REMOTE_SSH_USER=""
+REMOTE_SSH_HOST=""
+REMOTE_SSH="${REMOTE_SSH_USER}@${REMOTE_SSH_HOST}"
+
+# Remote-MySQL
+REMOTE_DB_NAME=""
+
+# Lokale MySQL
+LOCAL_DB_NAME=""
+LOCAL_DB_PASS=""
+
+# Optional: MySQL-User explizit angeben (wenn du kein ~/.my.cnf nutzt)
+REMOTE_DB_USER=""
+LOCAL_DB_USER=""
+
+DUMP_FILE="dump_$(date +%Y%m%d_%H%M%S).sql"
+
+echo "Erstelle Dump-Datei ${DUMP_FILE}..."
+
+ssh "${REMOTE_SSH}" "mysqldump \
+  -u ${REMOTE_DB_USER} \
+  -p'password' \
+  --single-transaction \
+  --quick \
+  --lock-tables=false \
+  --no-tablespaces \
+  --ignore-table=${REMOTE_DB_NAME}.wp_kunstmueoptions \
+  \"${REMOTE_DB_NAME}\"" > "${DUMP_FILE}"
+
+echo "Importiere ${DUMP_FILE} in lokale DB ${LOCAL_DB_NAME}..."
+mysql  -u"${LOCAL_DB_USER}" -p \
+  "${LOCAL_DB_NAME}" < "${DUMP_FILE}"
+
+echo "Fertig ✅"
+echo "Removing Dumpfile"
+rm ${DUMP_FILE}
